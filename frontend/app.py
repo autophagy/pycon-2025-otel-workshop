69998768f717bc6e0271810ffa2dcf012abdbbe7
Emily Woods <hello@emilywoods.me>
2025-03-30 17:58:43 +0200

    Wire up the frontend and backend services (#5)
    Co-authored-by: Mika Naylor <mail@autophagy.io>

--- frontend/app.py
+++ frontend/app.py
@@ -1,54 +1,33 @@
+import os
 import requests
 from flask import Flask, render_template, request
-from geopy.geocoders import Nominatim
-from geopy.distance import geodesic
-
-# gateway_url = "http://localhost:3001?q={place}"
-gateway_url = "http://api.open-notify.org/iss-now.json"
 
 app = Flask(__name__)
 
-def get_iss_coordinates():
-    r = requests.get(gateway_url)
-
-    if r.status_code == 200:
-        return (
-            r.json()["iss_position"]["latitude"],
-            r.json()["iss_position"]["longitude"],
-        )
-
-    return 0, 0
-
-def get_coordinates(place):
-    geolocator = Nominatim(user_agent="geo_app")
-    location = geolocator.geocode(place)
-    
-    if location:
-        return location.latitude, location.longitude
-    return None, None
-
-def calculate_distance(lat, long, issLat, issLong):
-    coords_place = (lat, long)
-    coords_iss = (issLat, issLong)
-    distance = geodesic(coords_iss, coords_place).km
-    return round(distance, 2)
-
 @app.route("/", methods=["GET", "POST"])
 def homepage():
-    lat = long = dist = place = None
-    issLat, issLong = get_iss_coordinates()
+    lat = long = dist = location = None
 
     if request.method == 'POST':
-        place = request.form['place']
+        location = request.form['location']
 
-        issLat, issLong = get_iss_coordinates()
-        lat, long = get_coordinates(place)
-        dist = calculate_distance(lat, long, issLat, issLong)
+        r = requests.get(app.config["GATEWAY_URL"], params={"location": location})
+        j = r.json()
+        dist = j.get("distance")
+        lat = j.get("location", {}).get("latitude")
+        long = j.get("location", {}).get("longitude")
     return (
-        render_template("index.html", latitude=issLat, longitude=issLong, distance=dist, place=place),
+        render_template("index.html", latitude=lat, longitude=long, distance=dist, location=location),
         200,
     )
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    gateway_url = os.getenv("GATEWAY_URL")
+    if gateway_url:
+        app.config.update(
+            GATEWAY_URL=gateway_url,
+        )
+        app.run(host="0.0.0.0")
+    else:
+        raise RuntimeError("GATEWAY_URL not defined")
