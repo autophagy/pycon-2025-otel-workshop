1e992d455fb484cb0890faa0663cbf2bdcef6112
Mika Naylor <mail@autophagy.io>
2025-03-30 18:19:00 +0200

    Propagate errors from backend services to frontend

--- frontend/app.py
+++ frontend/app.py
@@ -6,18 +6,23 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
-    lat = long = dist = location = None
+    lat = long = dist = location = err = None
 
     if request.method == 'POST':
         location = request.form['location']
 
         r = requests.get(app.config["GATEWAY_URL"], params={"location": location})
-        j = r.json()
-        dist = j.get("distance")
-        lat = j.get("location", {}).get("latitude")
-        long = j.get("location", {}).get("longitude")
+
+        if r.status_code == 200:
+            j = r.json()
+            dist = j.get("distance")
+            lat = j.get("location", {}).get("latitude")
+            long = j.get("location", {}).get("longitude")
+        else:
+            err = r.text
+            lat = long = dist = location = None
     return (
-        render_template("index.html", latitude=lat, longitude=long, distance=dist, location=location),
+        render_template("index.html", latitude=lat, longitude=long, distance=dist, location=location, error=err),
         200,
     )
 
